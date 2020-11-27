70007d13aa158f31e6eb24712f3b3ca8747fbd34
Devjoyti <devjoytib@gmail.com>
2020-11-27 21:32:16 +0530

    Added HandShaking Animation

---
+++ Day-1/HandShaking animation/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './App.css'
+import PanToolIcon from '@material-ui/icons/PanTool'
+
+
+const App = () => {
+  
+  return (
+      
+    <div className="App">
+      <PanToolIcon/>
+    </div>
+        
+  )
+} 
+
+export default App
